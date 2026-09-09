133ed223a12fd204f519f4295143434628f6bff4
Amanda-Ros4 <amandalima070811@gmail.com>
2026-09-09 09:18:04 -0300

    Enhance table header layout in People index component
    Updated the People/Index.vue component to improve the layout of the table header. Adjusted the class attributes for the actions header to utilize flexbox for better alignment and added a margin to the action label for improved visual clarity. These changes aim to enhance the overall user interface and user experience.

--- resources/js/Pages/People/Index.vue
+++ resources/js/Pages/People/Index.vue
@@ -110,9 +110,10 @@ function deletePerson() {
                                 <TableHead>
                                     {{ trans('people.fields.email') }}
                                 </TableHead>
-                                <TableHead class="w-28 text-right">
-                                    {{ trans('common.actions') }}
+                                <TableHead class="w-28 flex justify-end items-center">
+                                    <span class="mr-4">{{ trans('common.actions') }}</span>
                                 </TableHead>
+
                             </TableRow>
                         </TableHeader>
                         <TableBody>
