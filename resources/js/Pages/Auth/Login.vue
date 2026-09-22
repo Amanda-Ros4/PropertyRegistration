e9e4a20d52fff3a51b13c74cfccb13143385aa41
Amanda-Ros4 <amandalima070811@gmail.com>
2026-09-22 10:41:56 -0300

    Refactor Login component for improved code clarity and layout consistency
    Removed outdated comments and adjusted the formatting of the password input section in Login.vue. Enhanced the layout by ensuring proper spacing and alignment of elements, contributing to a cleaner and more user-friendly interface. These changes aim to improve code readability and maintain a cohesive design across the application.

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -57,12 +57,11 @@ const submit = () => {
 
             <div class="mt-4">
                 <InputLabel for="password" :value="trans('auth.password')" />
-                
-                <!-- ESTA É A DIV RELATIVA QUE PRENDE O BOTÃO DENTRO DO INPUT -->
+
                 <div class="relative mt-1">
                     <TextInput id="password" v-model="form.password" :type="showPassword ? 'text' : 'password'"
                         class="block w-full pr-10" required autocomplete="current-password" maxlength="100" />
-                    
+
                     <button type="button"
                         class="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-500 hover:text-gray-700"
                         @click="showPassword = !showPassword">
@@ -75,14 +74,14 @@ const submit = () => {
                         </svg>
 
                         <!-- Ícone de olho com risco (quando a senha está visível) -->
-                        <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" class="w-5 h-5">
+                        <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
                         </svg>
                     </button>
                 </div>
-                
+
                 <InputError class="mt-2" :message="form.errors.password" />
             </div>
 
